9df9b63e28740237317bdaf1e8b399487f09d100
Daksh Sardana <daksh2003sardana@gmail.com>
2023-05-12 18:34:55 +0530

    Website Code

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,4 +1,24 @@
-function nextSequence(){
-    
+// Arrays Created
+var buttonColors=["red", "blue", "green", "yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
 
+
+
+
+
+
+// Function to choose random number
+function nextSequence(){
+    var randomNumber= Math.floor(Math.random() * 4);
+    var randomChosenColour= buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour);
+    
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+// Function to play sound
+function playSound(){
+    var audio = new Audio("sounds/" + name + ".mp3");
+audio.play();
 }
