bd8b531966d51abbf03538dfaacf7091df8f0f37
Daksh Sardana <daksh2003sardana@gmail.com>
2023-06-10 13:20:42 +0530

    Website Code

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -3,13 +3,63 @@ var buttonColors=["red", "blue", "green", "yellow"];
 var gamePattern=[];
 var userClickedPattern=[];
 
+var started= false;
+var level=0;
+
+$(document).keypress(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
 
 
+$(".btn").click(function() {
+    var userChosenColour=$(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+});
 
+function checkAnswer(currentLevel) {
 
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      //2. Call startOver() if the user gets the sequence wrong.
+      startOver();
+    }
+
+}
 
 // Function to choose random number
 function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber= Math.floor(Math.random() * 4);
     var randomChosenColour= buttonColors[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -17,8 +67,26 @@ function nextSequence(){
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
+
 // Function to play sound
-function playSound(){
-    var audio = new Audio("sounds/" + name + ".mp3");
-audio.play();
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+// Function for animation
+function animatePress(currentColor){
+  $("#"+currentColor).addClass("pressed");
+  setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+//1. Create a new function called startOver().
+function startOver() {
+
+  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
